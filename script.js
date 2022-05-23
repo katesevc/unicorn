50a3d4490d192c1946fa4502b363b8687fa07ab5
Katerina Sevcikova <katerinasevcikova@hotmail.cz>
2022-05-23 15:31:01 +0200

    start

---
+++ script.js
@@ -0,0 +1,24 @@
+import { conversation as conv } from './chat'
+
+console.log(conv);
+
+document.addEventListener() => {
+    const target = document.querySelector('. conversation')
+}
+
+
+data.forEach(message, index) => {
+    setTimeout() => {
+    target.innerHTML += `
+    <div class="message message--${message.side}">
+        <div message="message_text">
+        ${message.text}
+
+        </div>
+    </div>
+`;
+    }, index * 500);
+
+});
+})
+
